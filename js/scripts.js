827c1694f54e6f55dd7b48fb4f98ee752825e199
Maria Lagunero <maria58@gmail.com>
2019-11-21 20:28:09 -0800

    add more function to page through css as well as added divs and ids to html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $("img").show();
+  $(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
 });

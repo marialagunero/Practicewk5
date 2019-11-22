7fd577cd61c76c32e572c92542258f2333f327cd
Maria Lagunero <maria58@gmail.com>
2019-11-21 20:02:10 -0800

    add new look for Peek-a-boo, new html, css, and scripts. Wrote over HTML help.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
-
-jQuery("p").click(function(){
-  alert("This is a paragraph.");
-});
-
-jQuery("img").click(function() {
-  alert("This is an image.")
+$(document).ready(function() {
+  $("p").click(function() {
+    $("img").show();
+  });
 });

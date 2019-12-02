aacc2f8f7e07353a80e08f69c8989586fafbebc2
Maria Lagunero <maria58@gmail.com>
2019-12-02 14:25:43 -0800

    add more js so that colors change with multiple clicks

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
     $("#initially-showing").toggle();
     $("#initially-hidden").toggle();
   });
-  $("button#blue").click(function(){
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+  $("button#pink").click(function() {
+    $("body").removeClass();
+    $("body").addClass("pink-background");
   });
 });
